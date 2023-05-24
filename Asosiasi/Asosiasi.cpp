1cc3d8113c8331f8ec6a9f3378392da3b52d4872
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-24 16:12:01 +0700

    menambahkan method dokter cetak pasien untuk mengecek pasien yang ada

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -44,9 +44,17 @@ void pasien::cetakDokter() {
     cout << endl;
 }
 void dokter::tambahPasien(pasien* pPasien) {
-    daftar_pasien.push_back(pasien);
+    daftar_pasien.push_back(pPasien);
     pPasien->tambahDokter(this);
 }
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter\"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
