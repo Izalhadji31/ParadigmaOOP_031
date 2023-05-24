9da2dffd47182604c827437a2c8ea36b7907de3a
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-24 16:05:09 +0700

    memasukan method cetak dokter untuk mengetahui dokter yang sedang bekerja

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -28,12 +28,20 @@ public:
     ~dokter() {
         cout << "Dokter \"" << nama << "\" tidak ada\n";
     }
-    ~dokter() {
-        cout << "Dokter \"" << nama << "\" tidak ada\n";
-    }
+    
     void tambahPasien(pasien*);
     void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien\"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
