4551857af2aae51961e409a874b8dcd0fc2f8ee5
Izalhadji31 <ryzalhadji@gmail.com>
2023-05-24 15:27:27 +0700

    memasukan pelajar

--- ParadigmaOOP_031.cpp
+++ ParadigmaOOP_031.cpp
@@ -22,7 +22,13 @@ public:
 
 class pelajar : public orang {
 public:
+    string sekolah;
 
+    pelajar(string pNama, string pSekolah) :|
+        orang(pNama),
+        sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
 };
 int main()
 {
